98190788125cccc21be1458ba4c2ace33c6ee2c2
px1099 <e0196678@u.nus.edu>
2019-04-12 15:46:54 +0800

    Add test file

---
+++ tests.py
@@ -0,0 +1,13 @@
+from pypokerengine.api.game import setup_config, start_poker
+from agent22player import Agent22Player
+
+#TODO:config the config as our wish
+config = setup_config(max_round=100, initial_stack=1000, small_blind_amount=10)
+
+
+
+config.register_player(name="FT1", algorithm=Agent22Player())
+config.register_player(name="FT2", algorithm=Agent22Player())
+
+
+game_result = start_poker(config, verbose=0)
